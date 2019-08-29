d65ad0bab61374211ead79a52a6a8a09a483839a
junjiejia <38582926+junjiejia@users.noreply.github.com>
2019-08-29 14:49:15 +0800

    Update seed.groovy

--- jobs/seed.groovy
+++ jobs/seed.groovy
@@ -1,1 +1,23 @@
-
+job('Seed Job') {
+  wrappers {
+    preBuildCleanup()
+  }
+  scm {
+    git {
+      branch('master')
+      remote {
+        name('upstream')
+        // replace this with whever you put this repo
+        url('https://github.com/junjiejia/job-dsl/master/')
+      }
+    }
+  }
+  steps {
+    // running tests first prevents half deployments which could break
+    gradle 'clean test'
+    dsl {
+      // any job ending in Job.groovy will be deployed
+      external 'jobs/piplineJob-dsl.groovy'
+    }
+  }
+}
